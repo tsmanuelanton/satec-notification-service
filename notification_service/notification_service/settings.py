1a9451168520844011addc0ff660de4dacf1643c
manuel_anton_satec <manuel.anton@satec.es>
2022-10-10 12:06:04 +0200

    MongoDB -> SQLite

--- notification_service/notification_service/settings.py
+++ notification_service/notification_service/settings.py
@@ -81,8 +81,8 @@ WSGI_APPLICATION = 'notification_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'db-satec-notification-service',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
