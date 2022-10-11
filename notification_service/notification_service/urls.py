bebc8bc7c847046e4644722498485b8d4757f230
manuel_anton_satec <manuel.anton@satec.es>
2022-10-11 10:24:24 +0200

    Autenticación en el sistema Issue #2 y panel admin

--- notification_service/notification_service/urls.py
+++ notification_service/notification_service/urls.py
@@ -1,9 +1,9 @@
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import path, include
 from api import urls as api_urls
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('v1/', include(api_urls)),
 ]
