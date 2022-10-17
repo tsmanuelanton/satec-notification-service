f34da736798e90531f9bd3ba22a7d3c628acc379
manuel_anton_satec <manuel.anton@satec.es>
2022-10-17 11:07:27 +0200

    Issue #4 Tests: Put service; Arreglo endpoint tests details service

--- notification_service/api/tests/views/services/test_update.py
+++ notification_service/api/tests/views/services/test_update.py
@@ -1,36 +1,42 @@
+from xmlrpc import server
 from rest_framework.test import APIRequestFactory, APITestCase, force_authenticate
 from api.views.services_views import ServicesDetailsApiView
 from rest_framework import status
 from api.tests.views.util import create_service, create_authenticated_user
 from api.serializers import ServicesSerializer
+from rest_framework.serializers import ErrorDetail
 
 endpoint = "/v1/services"
 
 
-class TestDetailsServices(APITestCase):
+class TestUpdateServices(APITestCase):
 
     def setUp(self) -> None:
         self.factory = APIRequestFactory()
 
-    def test_services_details_null(self):
-        '''Comprueba que se lanza un error cuando no existe la id del servicio'''
+    def test_services_update_empty(self):
+        '''Comprueba que se lanza un error cuando se envía el nuevo servicio vacío'''
 
         # Creamos un nuevo usario autenticado
         user, token = create_authenticated_user()
 
-        # Apuntamos el endpoint con el método get
-        request = self.factory.get(endpoint + "/1")
+        # Creamos el servicio a actualizar
+        service = create_service(token)
+        service.save()
+
+        # Apuntamos el endpoint con el método put y un cuerpo vacío
+        request = self.factory.put(f'{endpoint}/{service.id}', data={})
 
         force_authenticate(request, user, token)
 
         # Llamamos a la vista
-        response = ServicesDetailsApiView.as_view()(request, service_id=1)
+        response = ServicesDetailsApiView.as_view()(request, service_id=service.id)
 
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
-            response.data, {"res": f"Servicio con id 1 no existe"})
+            response.data, {'service_name': [ErrorDetail(string='This field may not be null.', code='null')]})
 
-    def test_services_details_owner(self):
+    def test_services_update_valid(self):
         '''Comprueba que se muestran los datos cuando el servicio pertenece al usuario'''
 
         # Creamos un nuevo usario autenticado con un servicio
@@ -38,8 +44,10 @@ class TestDetailsServices(APITestCase):
         my_service = create_service(token)
         my_service.save()
 
-        # Apuntamos el endpoint con el método get
-        request = self.factory.get(f'{endpoint}/{my_service.id}')
+        # Apuntamos el endpoint con el método put y el campo service_name actualizado
+        request = self.factory.put(f'{endpoint}/{my_service.id}', {
+            "service_name": "name",
+        })
 
         force_authenticate(request, user, token)
 
@@ -49,9 +57,9 @@ class TestDetailsServices(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(
-            response.data, ServicesSerializer(my_service).data)
+            dict(response.data)["service_name"], "name")
 
-    def test_services_details_not_owner(self):
+    def test_services_update_not_owner(self):
         '''Comprueba que se lanza un error cuando el servicio no pertene al usuario'''
 
         # Creamos otro usuario con un servicio
@@ -59,12 +67,16 @@ class TestDetailsServices(APITestCase):
         not_owned_service = create_service(other_token)
         not_owned_service.save()
 
-        request = self.factory.get(f'{endpoint}/{not_owned_service.id}')
-
         # Creamos un nuevo usario autenticado con un servicio
         user, token = create_authenticated_user()
         my_service = create_service(token)
         my_service.save()
+
+        # Apuntamos el endpoint con el método put a un servicio que no somos dueños
+        request = self.factory.get(f'{endpoint}/{not_owned_service.id}', {
+            "service_name": "name",
+        })
+
         force_authenticate(request, user, token)
 
         # Llamamos a la vista
