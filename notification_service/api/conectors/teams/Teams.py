3d4873d49314a4f8f2e883f3b5ea2354c656487b
manuel_anton_satec <manuel.anton@satec.es>
2023-01-31 13:18:37 +0100

    Teams conector: autenticación usario y contrasla

--- notification_service/api/conectors/teams/Teams.py
+++ notification_service/api/conectors/teams/Teams.py
@@ -21,8 +21,16 @@ class TeamsConector(IConector):
                 **meta
             }
 
+            tenant_id = data['subscription_data']['tenant_id']
+            client_id = data['subscription_data']['client_id']
+            email = data['subscription_data']['email']
+            password = data['subscription_data']['password']
+
+            token = TeamsConector.get_token(
+                email, password, client_id,  tenant_id)
+
             headers = {
-                "Authorization": "Bearer " + data['subscription_data']["token"],
+                "Authorization": "Bearer " + token,
                 "Content-Type": "application/json"
             }
 
@@ -33,7 +41,7 @@ class TeamsConector(IConector):
             res = requests.post(endpoint, json=body, headers=headers)
             if not res.ok:
                 print(res.json())
-                return res.json()
+            return res.json()
 
         except BaseException as e:
             print(e)
@@ -42,3 +50,27 @@ class TeamsConector(IConector):
 
     def get_subscription_serializer():
         return SubcriptionDataTeams
+
+    # https://learn.microsoft.com/en-us/graph/auth/auth-concepts
+    def get_token(email: str, password: str, client_id: str, tenant_id: str):
+        '''Obtiene token de acceso mediante autenticación por usario y contraseña'''
+
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
+
+        body = {
+            "client_id": client_id,
+            "scope": "https://graph.microsoft.com/.default",
+            "username": email,
+            "password": password,
+            "grant_type": "password"
+        }
+
+        url = f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token"
+        res = requests.post(url, body, headers=headers)
+
+        if res.ok:
+            return res.json().get("access_token")
+
+        raise BaseException(res.json())
--- notification_service/api/conectors/teams/serializers.py
+++ notification_service/api/conectors/teams/serializers.py
@@ -5,4 +5,7 @@ class SubcriptionDataTeams(serializers.Serializer):
     '''Valida el campo subsription data de la suscripción '''
     channel_id = serializers.CharField()
     team_id = serializers.CharField()
-    token = serializers.CharField()
+    tenant_id = serializers.CharField()
+    client_id = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField()
