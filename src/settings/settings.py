7326490bcc0e6114a76b7daaaa7670df35cdd571
manuel_anton_satec <manuel.anton@satec.es>
2023-02-08 14:27:57 +0100

    Refencias arregladas

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'notification_service.urls'
+ROOT_URLCONF = 'settings.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'notification_service.wsgi.application'
+WSGI_APPLICATION = 'settings.wsgi.application'
 
 
 # Database
