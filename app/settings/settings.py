21da98cde1d5fe2447353314377be5daf6eb8340
UO276213 <uo276213@uniovi.es>
2023-06-10 18:42:20 +0200

    volumen para DB y github secret para los env

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': env("DJANGO_DATABASE", default=BASE_DIR / 'db.sqlite3'),
     }
 }
 
