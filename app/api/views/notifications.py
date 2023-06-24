3ff27f2e0b0fae7886f423b51f791828830145bf
UO276213 <uo276213@uniovi.es>
2023-06-24 15:14:35 +0200

    Numerosas mejoras async y reestructurado

--- app/api/views/notifications.py
+++ app/api/views/notifications.py
@@ -1,20 +1,18 @@
-from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-from api.models import Conector, Subscription
+from api.models import Service, Subscription
 from api.serializers import MessageSerializer
 from api.util import has_permissions, import_conectors
-from api.views.services import get_service
 
 import logging
 logger = logging.getLogger("file_logger")
 
 import asyncio
-from asgiref.sync import async_to_sync
+from adrf.views import APIView
 
 class NotificationDetails(APIView):
 
-    def post(self, request, *args, **kwargs):
+    async def post(self, request,  *args, **kwargs):
         '''
         Envía el mensaje recibido al conector aducado
         '''
@@ -24,8 +22,7 @@ class NotificationDetails(APIView):
             return Response(msgSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         service_id = request.data.get('service')
-
-        service = get_service(service_id)
+        service = await Service.objects.select_related().aget(id=service_id)
         if not service:
             return Response({"res": "Unknown service."}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -36,62 +33,62 @@ class NotificationDetails(APIView):
             )
         
         # Enviamos las notificaciones a los suscriptores
-        successfull_msgs, not_successfull_msgs, fails = notify_subscriptors(
-            msgSerializer, service)
-        
-        logger.info(
-            f"El servicio {service.name} con id {service.id} ha enviado {successfull_msgs} notificaciones exitosas y han fallado {not_successfull_msgs}.")
+        successful, fails = await notify_subscriptors(
+            msgSerializer.data,
+                service)
 
-        return Response({"res": f"Se han enviado {successfull_msgs} notificaciones exitosas y han fallado {not_successfull_msgs}.",
-                         "fallos": fails}, status=status.HTTP_200_OK)
+        logger.info(
+            f"El servicio {service.name} con id {service.id} ha enviado {len(successful)} notificaciones exitosas y han fallado {len(fails)}.")
+
+        return Response({"res": f"Se han enviado {len(successful)} notificaciones exitosas y han fallado {len(fails)}.",
+                            "enviados": successful,
+                            "fallos": fails}, status=status.HTTP_200_OK)
+
     
-async def send_data_to_conector(data, conector: Conector):
+async def notify_subscriptor(subscription,message, options):
     '''Obtinene los conectores cargados y envía los datos al conector adecuado'''
     try:
-        options = data["options"].get(str(conector.id), {})
-        available_conectors = import_conectors("api/conectors")
+        conector = subscription.conector
+        available_conectors = import_conectors()
+
+        data = {
+            "subscription_data": subscription.subscription_data,
+            "message":  message,
+        }
         for available_con in available_conectors:
             if conector.name == available_con.getDetails().get("name"):
-                return (await available_con.notify(data, options)), conector
+                return subscription, (await available_con.notify(data, options))
     except BaseException as e:
-        return str(e), conector
-        
+        return subscription, str(e)
 
-@async_to_sync
-async def notify_subscriptors(msgSerializer, service):
+
+async def notify_subscriptors(notification_req, service):
     '''Envía las notificaciones a los suscriptores del servicio'''
-    fails = []
-    tasks = [] # Almacena las tareas send_data_to_conector
-    successfull_msgs, not_successfull_msgs = 0, 0
-
+    successes, fails = [], []
     subscriptions = Subscription.objects.filter(
-        service=service).select_related("conector") # Obtenemos las suscripciones del servicio y sus conectores
+        service=service).select_related("conector", "group") # Obtenemos las suscripciones del servicio y sus conectores
     
-    async for subscription in subscriptions:
-        conector = subscription.conector
-        if len(msgSerializer.data["restricted_to"]) > 0:
-            if conector.id not in msgSerializer.data["restricted_to"]:
-                continue
-        data = {
-            "subscription_id": subscription.id,
-            "subscription_data": subscription.subscription_data,
-            "message":  msgSerializer["message"].value,
-            "options": msgSerializer["options"].value
-        }
+    tasks = [] # Almacena las tareas send_data_to_conector
 
-        coroutine = send_data_to_conector(data, conector)
-        tasks.append(asyncio.create_task(coroutine))
-    
+    async for subscription in subscriptions:
+        if len(notification_req["restricted_to_groups"]) > 0:
+            if subscription.group == None or subscription.group.id not in notification_req["restricted_to_groups"]:
+                continue
+        message = notification_req["message"]
+        conector_options =  notification_req["options"].get(str(subscription.conector.id))
+
+        coroutine = notify_subscriptor(subscription,message, conector_options)
+        tasks.append(coroutine)
+
     for task in asyncio.as_completed(tasks):
-        error_info, conector =  await task # Obtenemos el resultado de la tarea send_data_to_conector
+        subscription, error_info =  await task # Obtenemos el resultado de la tarea send_data_to_conector
+        notification_context = {
+            "subscription_id": subscription.id,
+            "conector_name": subscription.conector.name
+        }
         if not error_info:
-            successfull_msgs += 1
+            successes.append(notification_context)
         else:
-            not_successfull_msgs += 1
-            notification_context = {
-                "subscription_id": subscription.id,
-                "conector_name": conector.name
-            }
             info = {
                 "service_name": service.name,
                 "service_id": service.id,
@@ -102,4 +99,4 @@ async def notify_subscriptors(msgSerializer, service):
             logger.error(f"Error al notificar - {info}")
             fails.append({**notification_context, "description": error_info})
 
-    return successfull_msgs, not_successfull_msgs, fails
+    return successes, fails
