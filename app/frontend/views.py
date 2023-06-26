3969618e165b8f97588da88f750de3caeb73dcd9
UO276213 <uo276213@uniovi.es>
2023-06-26 18:50:57 +0200

    Página de formulario no funcional

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse({"Hello, world. You're at the frontend index."})
+    return render(request, "frontend/index.html")
